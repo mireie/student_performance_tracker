bc75057bd960030b180a0b3db3cc2280d746fab8
mireie <michael.reiersgaard@gmail.com>
2021-10-01 14:01:00 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
   devise_for :users
+  match 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
   resources :students do
     resources :progresses
     resources :benchmark_results
