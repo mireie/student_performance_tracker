ec7e5ee49865e13d45d67aceb853db9b15771269
mireie <michael.reiersgaard@gmail.com>
2021-10-01 11:25:08 -0700

    Updating landing page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,8 @@
 class LandingController < ApplicationController
   def index
     @teachers = Teacher.where(:active => true)
+    @students = Student.where(:active => true)
+    @inactive_users = User.where(:active => false)
+    @users = User.where(:active => true)
   end
 end
