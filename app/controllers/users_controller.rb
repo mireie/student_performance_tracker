1cdfbb83a95f6d875179284df891ce4ae3133658
mireie <michael.reiersgaard@gmail.com>
2021-10-01 12:20:42 -0700

    prevent self deleting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :authorize_admin 
+  before_action :authorize_admin
 
   def index
     @users = User.all
@@ -13,23 +13,32 @@ class UsersController < ApplicationController
 
   def toggle_active
     @user = User.find(params[:id])
-    if @user.active?
-      @user.update(:active => false)
-      flash[:notice] = "User was successfully deactivated." 
+    unless @user.id === current_user.id
+      if @user.active?
+        @user.update(:active => false)
+        flash[:notice] = "User was successfully deactivated."
+      else
+        @user.update(:active => true)
+        flash[:notice] = "User was successfully activated."
+      end
     else
-      @user.update(:active => true)
-      flash[:notice] = "User was successfully activated."
+      flash[:alert] = "You cannot disable your own account."
     end
     redirect_to "/user-admin"
   end
 
   def destroy
     @user = User.find(params[:id])
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to "/user-admin", notice: "User was successfully removed." }
-      format.json { head :no_content }
+
+    unless @user.id === current_user.id
+      @user.destroy
+      respond_to do |format|
+        format.html { redirect_to "/user-admin", notice: "User was successfully removed." }
+        format.json { head :no_content }
+      end
+    else
+      flash[:alert] = "You cannot delete your own account."
+      redirect_to "/user-admin"
     end
   end
-
 end
