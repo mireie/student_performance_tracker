26667fdf93a64701cbf7db8f121096fabf24ac15
Michael Reiersgaard <mreiersgaard@careacademy.com>
2022-11-25 20:40:09 -0800

    prepare for fly.io

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem "jbuilder", "~> 2.5"
 # gem 'bcrypt', '~> 3.1.7'
 gem "devise"
 gem "faker"
+gem "execjs"
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
--- db/migrate/20210824161508_update_benchmarks_columns.rb
+++ db/migrate/20210824161508_update_benchmarks_columns.rb
@@ -3,14 +3,14 @@ class UpdateBenchmarksColumns < ActiveRecord::Migration[5.2]
     remove_column :benchmarks, :nwf_cls
     remove_column :benchmarks, :nwf_wrc
     remove_column :benchmarks, :wrf
-    change_column :benchmarks, :ge, :float, {:precision=>4, :scale=>1}
-    change_column :benchmarks, :vocab_ge, :float, {:precision=>4, :scale=>1}
-    change_column :benchmarks, :reading_comp_ge, :float, {:precision=>4, :scale=>1}
+    change_column :benchmarks, :ge, :float, :precision=>4, :scale=>1
+    change_column :benchmarks, :vocab_ge, :float, :precision=>4, :scale=>1
+    change_column :benchmarks, :reading_comp_ge, :float, :precision=>4, :scale=>1
     change_column :benchmarks, :lnf, :integer
     change_column :benchmarks, :snf, :integer
     change_column :benchmarks, :orf_grade_lvl, :integer
     change_column :benchmarks, :orf_cwpm, :integer
-    change_column :benchmarks, :orf_accuracy, :float, {:precision=>3, :scale=>3}
+    change_column :benchmarks, :orf_accuracy, :float, :precision=>3, :scale=>3
   end
 end
 
