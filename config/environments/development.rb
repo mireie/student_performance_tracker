c8c17ad39ea66e4d7614aae94055f16d6685d423
Michael Reiersgaard <mreiersgaard@careacademy.com>
2022-12-03 13:14:57 -0800

    add password reset, fly CD

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,8 +30,25 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Mailer
+
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_options = { from: "michael.reiersgaard@gmail.com" }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    user_name: Rails.application.credentials.gmail[:email],
+    password: Rails.application.credentials.gmail[:password],
+    authentication: "plain",
+    enable_starttls_auto: true,
+  }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
 
   config.action_mailer.perform_caching = false
 
@@ -58,5 +75,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,26 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "student_performance_tracker_#{Rails.env}"
 
+  # Mailer
+
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_options = { from: "michael.reiersgaard@gmail.com" }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    user_name: Rails.application.credentials.gmail[:email],
+    password: Rails.application.credentials.gmail[:password],
+    authentication: "plain",
+    enable_starttls_auto: true,
+  }
+  config.action_mailer.default_url_options = { host: ENV["PROD_DEFAULT_URL"] }
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
